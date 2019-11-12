79df039b0192c89f1ef250dea9bf226b0bb27645
roms1975 <odggbo@gmail.com>
2019-11-12 12:00:19 +0300

    complited

--- get_form_data.php
+++ get_form_data.php
@@ -47,6 +47,9 @@
         if (move_uploaded_file($_FILES['img']['tmp_name'], $uploadfile)) {
             $mysqli->set_charset("utf8");
             $flname = 'images/' .basename($_FILES['img']['name']);
+            
+            make_preview($flname);
+            
             $sql = "INSERT INTO picters SET comm_id='$comm_id', pic_url='$flname'";
             
             if ($result = $mysqli->query($sql)) {
@@ -57,5 +60,30 @@
         } else {
             return false;
         }
+    }
+    
+    function make_preview($source) {
+        $width = 320;
+        $height = 240;
+        $rgb = 0xffffff;
+        $size = getimagesize($source);
+        $format = strtolower(substr($size['mime'], strpos($size['mime'], '/')+1));
+        $icfunc = "imagecreatefrom" . $format;
+        if (!function_exists($icfunc)) return false;
+        $x_ratio = $width / $size[0];
+        $y_ratio = $height / $size[1];
+        $ratio = min($x_ratio, $y_ratio);
+        $use_x_ratio = ($x_ratio == $ratio);
+        $new_width = $use_x_ratio  ? $width  : floor($size[0] * $ratio);
+        $new_height = !$use_x_ratio ? $height : floor($size[1] * $ratio);
+        $new_left = $use_x_ratio  ? 0 : floor(($width - $new_width) / 2);
+        $new_top = !$use_x_ratio ? 0 : floor(($height - $new_height) / 2);
+        $img = imagecreatetruecolor($width,$height);
+        imagefill($img, 0, 0, $rgb);
+        $photo = $icfunc($source);
+        imagecopyresampled($img, $photo, $new_left, $new_top, 0, 0, $new_width, $new_height, $size[0], $size[1]);
+        imagejpeg($img, $source);
+        imagedestroy($img);
+        imagedestroy($photo);
     }
 ?>
